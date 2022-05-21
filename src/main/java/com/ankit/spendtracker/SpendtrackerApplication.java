f85218a508b91ed68ed7687ea7b765f0e8dcd31b
Ankit Verma <ankitverma10203@gmail.com>
2022-05-21 22:43:06 +0530

    added the maven project

---
+++ src/main/java/com/ankit/spendtracker/SpendtrackerApplication.java
@@ -0,0 +1,13 @@
+package com.ankit.spendtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpendtrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpendtrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ankit/spendtracker/SpendtrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ankit.spendtracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpendtrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
