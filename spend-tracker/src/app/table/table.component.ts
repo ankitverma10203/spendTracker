fe0a5c21e3dfc6cf102bd1de1e696d67a009e955
Ankit Verma <ankitverma10203@gmail.com>
2025-03-06 01:46:56 +0530

    fix record deletion issue

--- spend-tracker/src/app/table/table.component.ts
+++ spend-tracker/src/app/table/table.component.ts
@@ -5,25 +5,30 @@ import { SpendTrackerDataModiferService } from '../service/spend-tracker-data-mo
 @Component({
   selector: 'app-table',
   templateUrl: './table.component.html',
-  styleUrls: ['./table.component.css']
+  styleUrls: ['./table.component.css'],
 })
 export class TableComponent implements OnInit {
-
   @Input() public fields: any = '';
   @Input() public date: any = '';
   @Input() public totalAmounts: any = '';
   @Input() public records: any = '';
 
-  constructor(private spendTrackerDataModiferService: SpendTrackerDataModiferService, private _snackBar: MatSnackBar) { }
+  constructor(
+    private spendTrackerDataModiferService: SpendTrackerDataModiferService,
+    private _snackBar: MatSnackBar
+  ) {}
 
-  ngOnInit(): void { }
+  ngOnInit(): void {}
 
-  public deleteRecord(id: string) : void {
-    console.log(this.records[this.date][0]);
-    this.spendTrackerDataModiferService.deleteRecord(id).subscribe((details) => {
-      console.log("response: ", details);
-      this._snackBar.open("Record Deletion", "Success", { duration: 2000 });
-    });
+  public deleteRecord(record: any): void {
+    const id: string = record['id'];
+    this.spendTrackerDataModiferService
+      .deleteRecord(id)
+      .subscribe((details) => {
+        this.records[this.date] = this.records[this.date].filter(
+          (rec: any) => rec['id'] != record['id']
+        );
+        this._snackBar.open('Record Deletion', 'Success', { duration: 2000 });
+      });
   }
-
 }
